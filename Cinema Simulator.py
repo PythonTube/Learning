5246c5a2f907e500123e5e7f4bdc463698f60bbf
Emre <67742162+PythonTube@users.noreply.github.com>
2020-07-05 17:13:14 +0300

    Create Cinema Simulator.py

---
+++ Cinema Simulator.py
@@ -0,0 +1,28 @@
+films = {
+    "Micky Mouse":[3,5],
+    "Bourne":[18,5],
+    "Tarzan":[15,5],
+    "Ghost Busters":[12,5]
+    }
+
+while True:
+    choice = input("What film would you like to watch?: ").strip().title()
+    
+    if choice in films:
+        age = int(input("How old are you?: ").strip())
+        
+        if age >= films[choice][0]: #check user age
+            
+            num_seats = films[choice][1]
+            
+            if num_seats > 0: #check enough seats
+                print("Enjoy the film!")
+                films[choice][1] = films[choice][1] - 1
+            else:
+                print("Sorry, we are sold out!")
+            
+        else:
+            print("You are too young to see that film!")
+            
+    else:
+        print("We don't have that film!")
